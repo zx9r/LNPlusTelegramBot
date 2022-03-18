25d0cdf3ce9bb570440aaaca5567ec4429ab0cf0
zx9r <alberto.devesa@gmail.com>
2022-03-18 19:15:11 +0100

    Added LN API endpoints

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ LNPLUS_URL = 'https://lightningnetwork.plus'
 LNPLUS_PENDING_PAGE1 = '/swaps?status=pending&shape=&size=&commit=Search'
 LNPLUS_SWAP_URL = 'https://lightningnetwork.plus/swaps/'
 
+LNPLUS_API_URL = 'https://lightningnetwork.plus/api/'
+LNPLUS_API_LATEST_SWAPS = 'swaps.json/'
+LNPLUS_API_SWAP_DETAILS = 'swap.json/'
+
 POLL_INTERVAL = 60
 
 MIN_CAPACITY = 1_000_000
